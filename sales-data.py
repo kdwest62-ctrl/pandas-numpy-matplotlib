53e07c031a3b0ce3668b4a418b50689921ac3e47
Kear Ibale <kdwest62@gmail.com>
2026-05-05 15:41:04 +0800

    Refactor sales data analysis logic
    Refactor sales data processing to use unique product names and calculate units sold.

--- sales-data.py
+++ sales-data.py
@@ -4,19 +4,20 @@ path = input("CSV path: ")
 df = pd.read_csv(path)
 print(df.to_string())
 
-print("Best seller")
-best_seller = df[df['units_sold'] == max(df['units_sold'].to_list())]
-print(best_seller.to_string())
+products = []
+for item in df['product_name'].to_list():
+    if item not in products:
+        products.append(item)
 
-a = set(df['product_name'].to_list())
 items_sold = []
-print(a)
-for item in a:
+for item in products:
     result = df.loc[df['product_name'] == item, 'units_sold']
     items_sold.append(sum(result))
-b = dict(zip(a, items_sold))
+b = dict(zip(products, items_sold))
 print(b)
 
 for k, v in b.items():
     if max(b.values()) == b[k]:
         print(f"Best seller: {k} with {v} units sold")
+    elif min(b.values()) == b[k]:
+        print(f"Low seller: {k} with {v} units sold")
