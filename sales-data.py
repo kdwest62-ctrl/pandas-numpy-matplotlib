e84444cd41931a66a29e49e7bd4f8e5f0a7f5836
Kear Ibale <kdwest62@gmail.com>
2026-05-06 06:41:51 +0800

    Implement customer product sales visualization
    Added customer analysis and visualization for products sold.

--- sales-data.py
+++ sales-data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 path = input("CSV path: ")
 df = pd.read_csv(path)
@@ -21,3 +22,22 @@ for k, v in b.items():
         print(f"Best seller: {k} with {v} units sold")
     elif min(b.values()) == b[k]:
         print(f"Low seller: {k} with {v} units sold")
+
+customers = []
+for item in df['customer_id'].to_list():
+   if item not in customers:
+       customers.append(item)
+
+products_bought = []
+for customer in customers:
+   result = df.loc[df['customer_id'] == customer, 'units_sold']
+   products_bought.append(sum(result))
+print(customers)
+print(products_bought)
+
+plt.bar(customers, products_bought)
+plt.title("Products Sold per Customer")
+plt.xlabel("Customers")
+plt.ylabel("Products Bought")
+plt.tight_layout()
+plt.show()
