ffdec6bd35c3b521731c783a1bd1364c189c458a
Kear Ibale <kdwest62@gmail.com>
2026-05-05 15:29:11 +0800

    Calculate total units sold for each product
    Added functionality to calculate total units sold for each product.

--- sales-data.py
+++ sales-data.py
@@ -7,3 +7,12 @@ print(df.to_string())
 print("Best seller")
 best_seller = df[df['units_sold'] == max(df['units_sold'].to_list())]
 print(best_seller.to_string())
+
+a = set(df['product_name'].to_list())
+items_sold = []
+print(a)
+for item in a:
+    result = df.loc[df['product_name'] == item, 'units_sold']
+    items_sold.append(sum(result))
+b = dict(zip(a, items_sold))
+print(b)
