7a9da11ab306cd245b9ad5393ccd45513786a204
Kear Ibale <kdwest62@gmail.com>
2026-05-06 13:59:33 +0800

    Update menu options and improve user interaction

--- football-data.py
+++ football-data.py
@@ -3,12 +3,24 @@ import matplotlib.pyplot as plt
 
 path = input("CSV path: ")
 df = pd.read_csv(path)
-
-print("1. Show CSV\n2. Totals\n3. Averages\n4. Simulation\n5. Graphs")
+print("1. Show CSV\n2. Statistical Analysis\n3. Charts\n4. Simulate remaining matches\n5. Exit")
 df['Goals_For'] = df['Goals_For'].astype('Int64')
 df['Goals_Against'] = df['Goals_Against'].astype('Int64')
-print(df.to_string())
-print('-'* 8)
+while True:
+    choice = input("Select option (input number):")
+    if choice == '1':
+        print(df.to_string())
+    elif choice == '2':
+
+    elif choice == '3':
+
+    elif choice == '4':
+
+    elif choice == '5':
+        break
+    else:
+        print("Option not available")
+
 results = df['Result (W/L/D)'].tolist()
 print(f"Team record [W: {results.count('W')} | L: {results.count('L')} | D: {results.count('D')}]")
 print(f"Total points: {results.count('W')*3 + results.count('D')*1}")
@@ -31,8 +43,8 @@ y = df['Result (W/L/D)'].dropna().tolist()
 x = df['Match_Number'].tolist()
 while len(x) > len(y):
     del x[-1]
-plt.plot(x, y, color="red", markersize=10,
-         marker=".", markerfacecolor="pink")
+plt.plot(x, y, color="blue", markersize=10,
+         marker=".", markerfacecolor="cyan")
 plt.title("Form Guide")
 plt.xlabel("Match Number")
 plt.ylabel("Result (W/L/D)")
