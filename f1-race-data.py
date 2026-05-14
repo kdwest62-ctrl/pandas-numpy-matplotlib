0e7070f4857bde5b09415fef9b5bdc24189e6b3e
Kear Ibale <kdwest62@gmail.com>
2026-05-14 10:50:50 +0800

    Add menu options for F1 race data selection

---
+++ f1-race-data.py
@@ -0,0 +1,28 @@
+print("Options")
+options = ["1. Statistics",
+           "2. Championship Points Progression",
+           "3. Grid Position & Final Position Correlation",
+           "4. Tire Degradation Curves",
+           "5. Weather Impact Analysis",
+           "6. Performance Comparison Between Teammates",
+           "7. Exit"]
+for option in options:
+    print(option)
+while True:
+    select = input("Select option (number): ")
+    if select == '1':
+        pass
+    elif select == '2':
+        pass
+    elif select == '3':
+        pass
+    elif select == '4':
+        pass
+    elif select == '5':
+        pass
+    elif select == '6':
+        pass
+    elif select == '7':
+        break
+    else:
+        print("Option unavailable")
