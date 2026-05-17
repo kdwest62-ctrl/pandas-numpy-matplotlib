fba7da88457d1e8806cb5f7141f17f97b5d98640
Kear Ibale <kdwest62@gmail.com>
2026-05-17 11:21:47 +0800

    Refactor driver list handling and add correlation calculation

--- f1-race-data.py
+++ f1-race-data.py
@@ -13,23 +13,31 @@ options = ["1. Statistics",
            "7. Exit"]
 for option in options:
     print(option)
+
+drivers = []
+driver_list = df['driver_code'].tolist()
+for item in driver_list:
+    if item not in drivers:
+        drivers.append(item)
 while True:
     select = input("Select option (number): ")
     if select == '1':
         print(df.to_string())
     elif select == '2':
-        drivers = []
-        driver_list = df['driver_code'].tolist()
-        for item in driver_list:
-            if item not in drivers:
-                drivers.append(item)
         for driver in drivers:
             driver_column = df[df['driver_code'] == driver]
             driver_points = driver_column['points_awarded'].tolist()
             cum_points = np.cumsum(np.array(driver_points))
             print(f"{driver} | {cum_points}")
     elif select == '3':
-        pass
+        for driver in drivers:
+            driver_column = df[df['driver_code'] == driver]
+            grid_pos = driver_column['starting_grid_position'].tolist()
+            final_pos = driver_column['final_position'].tolist()
+            correlation = np.corrcoef(grid_pos, final_pos)
+            has_nan = np.isnan(correlation).any()
+            print(f"{driver}\n{correlation}")
+            print('-' * 8)
     elif select == '4':
         pass
     elif select == '5':
