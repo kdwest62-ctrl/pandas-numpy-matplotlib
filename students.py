e3cde6447833f7a1b2268babf999aa51328da0f9
Kear Ibale <kdwest62@gmail.com>
2026-05-19 07:03:45 +0800

    Implement user menu for statistics and performance
    Added a menu for user options to view statistics and performance charts.

--- students.py
+++ students.py
@@ -2,15 +2,23 @@ import pandas as pd
 
 path = input("CSV path: ")
 df = pd.read_csv(path)
-print(df.to_string())
-print('-' * 8)
-print(df['class'].value_counts())
-print('-' * 8)
-class_a_males = ((df['gender'] == "M") & (df['class'] == 'Class A')).sum()
-class_a_females = ((df['gender'] == "F") & (df['class'] == 'Class A')).sum()
-class_b_males = ((df['gender'] == "M") & (df['class'] == 'Class B')).sum()
-class_b_females = ((df['gender'] == "F") & (df['class'] == 'Class B')).sum()
-print(f"Class A male students: {class_a_males}")
-print(f"Class A female students: {class_a_females}")
-print(f"Class B male students: {class_b_males}")
-print(f"Class B female students: {class_b_females}")
+print("1. Statistics\n2. Class Performance Heatmap\n3. Exit")
+while True:
+    option = input("Select option (number): ")
+    if option == '1':
+        print(df.to_string())
+    elif option == '2':
+        print("a. Students vs Subjects\nb. Performance Matrices\nc. Normalized Score")
+        chart = input("Select chart (letter): ")
+        if chart == 'a':
+            pass
+        elif chart == 'b':
+            pass
+        elif chart == 'c':
+            pass
+        else:
+            print("Chart not available")
+    elif option == '3':
+        break
+    else:
+        print("Option not available")
