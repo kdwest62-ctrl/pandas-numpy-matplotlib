4a7d307ad3d0e298568cd5dcc5c273194df266d1
Kear Ibale <kdwest62@gmail.com>
2026-05-24 10:24:07 +0800

    Add social media post analysis script

---
+++ social-media-posts.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+path = input("CSV path: ")
+df = pd.read_csv(path)
+print("Options")
+options = ["1. CSV",
+           "2. Engagement Stats",
+           "3. Reach Stats",
+           "4. 7-Day Trend of Average Likes",
+           "5. Exit"]
+for option in options:
+    print(option)
+while True:
+    select = input("Select option (number): ")
+    if select == '1':
+        print(df.to_string())
+    elif select == '2':
+        stats = ["a. Platform",
+                 "b. Content Type",
+                 "c. Weighted Score",
+                 "d. Engagement by Hour",
+                 "e. Weekday vs Weekend"]
+        for stat in stats:
+            print(stat)
+        choice = input("Choose stat (letter): ")
+        if choice == 'a':
+            pass
+        elif choice == 'b':
+            pass
+        elif choice == 'c':
+            pass
+        elif choice == 'd':
+            pass
+        elif choice == 'e':
+            pass
+        else:
+            print("Choice not available")
+    elif select == '3':
+        print("a. Platform\nb. Content Type")
+        choice = input("Choose stat (letter): ")
+        if choice == 'a':
+            pass
+        elif choice == 'b':
+            pass
+        else:
+            print("Choice not available")
+    elif select == '4':
+        pass
+    elif select == '5':
+        print("Program closed")
+        break
+    else:
+        print("Option not available")
