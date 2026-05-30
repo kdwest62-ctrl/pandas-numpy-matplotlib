c908959c98c710a3dc90390114b1c4bcb3c34fc8
Kear Ibale <kdwest62@gmail.com>
2026-05-30 11:45:26 +0800

    Refactor code structure

--- social-media-posts.py
+++ social-media-posts.py
@@ -12,39 +12,64 @@ options = ["1. CSV",
            "5. Exit"]
 for option in options:
     print(option)
-
 ig = df[df['platform'] == 'Instagram']
+ig_likes = ig['likes'].tolist()
+ig_shares = ig['shares'].tolist()
+ig_comments = ig['comments'].tolist()
+ig_reach = ig['reach'].tolist()
+
 fb = df[df['platform'] == 'Facebook']
+fb_likes = fb['likes'].tolist()
+fb_shares = fb['shares'].tolist()
+fb_comments = fb['comments'].tolist()
+fb_reach = fb['reach'].tolist()
+
 twt = df[df['platform'] == 'Twitter']
+twt_likes = twt['likes'].tolist()
+twt_shares = twt['shares'].tolist()
+twt_comments = twt['comments'].tolist()
+twt_reach = twt['reach'].tolist()
+
 img = df[df['content_type'] == 'Image']
+img_likes = img['likes'].tolist()
+img_shares = img['shares'].tolist()
+img_comments = img['comments'].tolist()
+img_reach = img['reach'].tolist()
+
 vid = df[df['content_type'] == 'Video']
+vid_likes = vid['likes'].tolist()
+vid_shares = vid['shares'].tolist()
+vid_comments = vid['comments'].tolist()
+vid_reach = vid['reach'].tolist()
+
 txt = df[df['content_type'] == 'Text']
+txt_likes = txt['likes'].tolist()
+txt_shares = txt['shares'].tolist()
+txt_comments = txt['comments'].tolist()
+txt_reach = txt['reach'].tolist()
+
+link_likes = df[df['content_type'] == 'Link']['likes'].values[0]
+link_shares = df[df['content_type'] == 'Link']['shares'].values[0]
+link_comments = df[df['content_type'] == 'Link']['comments'].values[0]
+link_reach = df[df['content_type'] == 'Link']['reach'].values[0]
+
+reel_likes = df[df['content_type'] == 'Reel']['likes'].values[0]
+reel_shares = df[df['content_type'] == 'Reel']['shares'].values[0]
+reel_comments = df[df['content_type'] == 'Reel']['comments'].values[0]
+reel_reach = df[df['content_type'] == 'Reel']['reach'].values[0]
+
+carousel_likes = df[df['content_type'] == 'Carousel']['likes'].values[0]
+carousel_shares = df[df['content_type'] == 'Carousel']['shares'].values[0]
+carousel_comments = df[df['content_type'] == 'Carousel']['comments'].values[0]
+carousel_reach = df[df['content_type'] == 'Carousel']['reach'].values[0]
 while True:
     select = input("Select option (number): ")
     if select == '1':
         print(df.to_string())
     elif select == '2':
-        stats = ["a. Platform",
-                 "b. Content Type",
-                 "c. Weighted Score",
-                 "d. Engagement by Time",
-                 "e. Weekday vs Weekend"]
-        for stat in stats:
-            print(stat)
+        print("a. Platform | b. Content Type | c. Weighted Score | d. Engagement by Time | e. Weekday vs Weekend")
         choice = input("Choose stat (letter): ")
         if choice == 'a':
-            ig_likes = ig['likes'].tolist()
-            ig_shares = ig['shares'].tolist()
-            ig_comments = ig['comments'].tolist()
-
-            fb_likes = fb['likes'].tolist()
-            fb_shares = fb['shares'].tolist()
-            fb_comments = fb['comments'].tolist()
-
-            twt_likes = twt['likes'].tolist()
-            twt_shares = twt['shares'].tolist()
-            twt_comments = twt['comments'].tolist()
-
             data = {'platform': ['Instagram', 'Facebook', 'Twitter'],
                     'likes': [sum(ig_likes), sum(fb_likes), sum(twt_likes)],
                     'shares': [sum(ig_shares), sum(fb_shares), sum(twt_shares)],
@@ -52,30 +77,6 @@ while True:
             engage_pl = pd.DataFrame(data)
             print(engage_pl.to_string())
         elif choice == 'b':
-            img_likes = img['likes'].tolist()
-            img_shares = img['shares'].tolist()
-            img_comments = img['comments'].tolist()
-
-            vid_likes = vid['likes'].tolist()
-            vid_shares = vid['shares'].tolist()
-            vid_comments = vid['comments'].tolist()
-
-            txt_likes = txt['likes'].tolist()
-            txt_shares = txt['shares'].tolist()
-            txt_comments = txt['comments'].tolist()
-
-            link_likes = df[df['content_type'] == 'Link']['likes'].values[0]
-            link_shares = df[df['content_type'] == 'Link']['shares'].values[0]
-            link_comments = df[df['content_type'] == 'Link']['comments'].values[0]
-
-            reel_likes = df[df['content_type'] == 'Reel']['likes'].values[0]
-            reel_shares = df[df['content_type'] == 'Reel']['shares'].values[0]
-            reel_comments = df[df['content_type'] == 'Reel']['comments'].values[0]
-
-            carousel_likes = df[df['content_type'] == 'Carousel']['likes'].values[0]
-            carousel_shares = df[df['content_type'] == 'Carousel']['shares'].values[0]
-            carousel_comments = df[df['content_type'] == 'Carousel']['comments'].values[0]
-
             data = {'content_type': ['Image', 'Video', 'Text', 'Link', 'Reel', 'Carousel'],
                     'likes': [sum(img_likes), sum(vid_likes), sum(txt_likes), link_likes, reel_likes, carousel_likes],
                     'shares': [sum(img_shares), sum(vid_shares), sum(txt_shares), link_shares, reel_shares,
@@ -139,25 +140,14 @@ while True:
         else:
             print("Choice not available")
     elif select == '3':
-        print("a. Platform\nb. Content Type")
+        print("a. Platform | b. Content Type")
         choice = input("Choose stat (letter): ")
         if choice == 'a':
-            ig_reach = ig['reach'].tolist()
-            fb_reach = fb['reach'].tolist()
-            twt_reach = twt['reach'].tolist()
             data = {'platform': ['Instagram', 'Facebook', 'Twitter'],
                     'reach': [sum(ig_reach), sum(fb_reach), sum(twt_reach)]}
             reach_pl = pd.DataFrame(data)
             print(reach_pl.to_string())
         elif choice == 'b':
-            img_reach = img['reach'].tolist()
-            vid_reach = vid['reach'].tolist()
-            txt_reach = txt['reach'].tolist()
-
-            link_reach = df[df['content_type'] == 'Link']['reach'].values[0]
-            reel_reach = df[df['content_type'] == 'Reel']['reach'].values[0]
-            carousel_reach = df[df['content_type'] == 'Carousel']['reach'].values[0]
-
             data = {'content_type': ['Image', 'Video', 'Text', 'Link', 'Reel', 'Carousel'],
                     'reach': [sum(img_reach), sum(vid_reach), sum(txt_reach), link_reach, reel_reach, carousel_reach]}
             reach_ct = pd.DataFrame(data)
